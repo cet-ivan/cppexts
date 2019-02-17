715c450499767dada6752914c6bd017304728cf8
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2019-02-17 01:33:28 +0100

    Added bitset

---
+++ source/bitset.h
@@ -0,0 +1,29 @@
+// Copyright Ivan Stanojevic 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// https://www.boost.org/LICENSE_1_0.txt)
+
+
+
+#ifndef __BITSET_H
+
+#define __BITSET_H
+
+
+
+#include <bitset>
+
+
+
+using std :: bitset ;
+
+using std :: operator & ;
+using std :: operator | ;
+using std :: operator ^ ;
+
+using std :: operator >> ;
+using std :: operator << ;
+
+
+
+#endif
