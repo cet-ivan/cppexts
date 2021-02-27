8063be47098b723f59be9167bd54212bb13ba0fb
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2021-02-27 23:54:16 +0100

    Refined normalize_congruence_ring_element

--- source/conring.h
+++ source/conring.h
@@ -1,4 +1,4 @@
-// Copyright Ivan Stanojevic 2014.
+// Copyright Ivan Stanojevic 2020.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
@@ -19,6 +19,7 @@
 #include "utility.h"
 
 #include "typeconv.h"
+#include "numbase.h"
 
 
 
@@ -48,6 +49,21 @@ if ( value < 0 )                                                             \
 }
 
 FOR_BUILTIN_SIGNED_INTEGRAL_TYPES(__DEFINE_NORMALIZE_CONGRUENCE_RING_ELEMENT)
+
+#undef __DEFINE_NORMALIZE_CONGRUENCE_RING_ELEMENT
+
+
+//
+
+#define __DEFINE_NORMALIZE_CONGRUENCE_RING_ELEMENT(Type)                     \
+                                                                             \
+inline void normalize_congruence_ring_element ( Type & value, Type modulus ) \
+                                                                             \
+{                                                                            \
+value %= modulus ;                                                           \
+}
+
+FOR_BUILTIN_UNSIGNED_INTEGRAL_TYPES(__DEFINE_NORMALIZE_CONGRUENCE_RING_ELEMENT)
 
 #undef __DEFINE_NORMALIZE_CONGRUENCE_RING_ELEMENT
 
