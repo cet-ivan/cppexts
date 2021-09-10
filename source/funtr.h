66753c94b805f77e5f4b4e0e4115a22df921c021
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2021-09-11 01:58:43 +0200

    Added empty_indexing

--- source/funtr.h
+++ source/funtr.h
@@ -1,4 +1,4 @@
-// Copyright Ivan Stanojevic 2016.
+// Copyright Ivan Stanojevic 2021.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
@@ -12,7 +12,6 @@
 
 
 #include "cstddef.h"
-
 #include "type_traits.h"
 
 
@@ -79,6 +78,99 @@ public:
   typedef typename remove_const < T > :: type value_type ;
 
 } ;
+
+
+
+// *** EMPTY_INDEXING ***
+
+
+//
+
+template < class T, size_t Dimension >
+class empty_indexing_imp
+
+{
+public:
+
+  empty_indexing_imp < T, Dimension - 1 > operator [ ] ( size_t ) const
+    { return empty_indexing_imp < T, Dimension - 1 > ( ) ; }
+
+} ;
+
+
+//
+
+template < class T >
+class empty_indexing_imp < T, 1 >
+
+{
+public:
+
+  T operator [ ] ( size_t ) const
+    { return T ( ) ; }
+
+} ;
+
+
+//
+
+template < class T >
+class empty_indexing_imp < T, 0 > ;
+
+
+//
+
+template < class T, size_t Dimension >
+class empty_indexing_traits
+
+{
+public:
+
+  typedef empty_indexing_imp < T, Dimension > type ;
+
+} ;
+
+
+//
+
+template < class T >
+class empty_indexing_traits < T, 0 >
+
+{
+public:
+
+  typedef typename remove_const < T > :: type type ;
+
+} ;
+
+
+//
+
+template < class T, size_t Dimension >
+using empty_indexing = typename empty_indexing_traits < T, Dimension > :: type ;
+
+
+//
+
+template < class T, size_t Dimension >
+class indexing_traits < empty_indexing_imp < T, Dimension > >
+
+{
+public:
+
+  typedef size_t index_type ;
+  typedef empty_indexing < T, Dimension - 1 > value_type ;
+
+  static constexpr size_t size ( empty_indexing < T, Dimension > )
+    { return 0 ; }
+
+} ;
+
+
+//
+
+template < class T >
+class indexing_traits < empty_indexing_imp < T, 0 > > ;
 
 
 
