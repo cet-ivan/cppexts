7b9d6a578f69867d854a45a73287911d9539c2b9
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2022-04-14 15:14:44 +0200

    Added sequence_size and concatenate

--- source/list.h
+++ source/list.h
@@ -1,4 +1,4 @@
-// Copyright Ivan Stanojevic 2018.
+// Copyright Ivan Stanojevic 2022.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
@@ -27,6 +27,85 @@ using std :: operator <= ;
 using std :: operator >= ;
 
 using std :: swap ;
+
+
+
+// *** SEQUENCE_SIZE ***
+
+
+template < class T, class Allocator >
+inline size_t sequence_size ( const list < T, Allocator > & x )
+
+{
+return x.size ( ) ;
+}
+
+
+
+// *** CONCATENATION ***
+
+
+template < class T, class Allocator >
+class list_concatenator
+
+{
+public:
+
+  list < T, Allocator > l ;
+
+  list_concatenator ( const list < T, Allocator > & i_l ) :
+    l ( i_l )
+    { }
+
+  list_concatenator ( list < T, Allocator > && i_l ) :
+    l ( move ( i_l ) )
+    { }
+
+  template < class Sequence >
+  list_concatenator & operator + ( const Sequence & s )
+    { l.insert ( l.end ( ), begin ( s ), end ( s ) ) ;
+      return * this ; }
+
+  template < class Sequence >
+  list_concatenator & operator + ( Sequence & s )
+    { l.insert ( l.end ( ), cbegin ( s ), cend ( s ) ) ;
+      return * this ; }
+
+  template < class Sequence >
+  list_concatenator & operator + ( Sequence && s )
+    { l.insert ( l.end ( ), make_move_iterator ( begin ( s ) ),
+                            make_move_iterator ( end   ( s ) ) ) ;
+      return * this ; }
+
+  list_concatenator & operator + ( list < T, Allocator > && s )
+    { l.splice ( l.end ( ), s ) ;
+      return * this ; }
+
+} ;
+
+
+//
+
+template < class T, class Allocator, class ... Sequences >
+inline list < T, Allocator >
+  concatenate ( const list < T, Allocator > & l, Sequences && ... s )
+
+{
+return move ( (         list_concatenator < T, Allocator > ( l )
+                + ... + forward < Sequences > ( s ) ).l ) ;
+}
+
+
+//
+
+template < class T, class Allocator, class ... Sequences >
+inline list < T, Allocator >
+  concatenate ( list < T, Allocator > && l, Sequences && ... s )
+
+{
+return move ( (         list_concatenator < T, Allocator > ( move ( l ) )
+                + ... + forward < Sequences > ( s ) ).l ) ;
+}
 
 
 
