413a2be0c36c27fcc9b7135ea3fc1291d193c6bb
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2025-03-25 03:25:37 +0100

    Changed sequence_size back to indexing_size

--- source/array.h
+++ source/array.h
@@ -1,4 +1,4 @@
-// Copyright Ivan Stanojevic 2022.
+// Copyright Ivan Stanojevic 2025.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
@@ -51,12 +51,10 @@ public:
 } ;
 
 
-
-// *** SEQUENCE_SIZE ***
-
+//
 
 template < class T, size_t N >
-inline constexpr size_t sequence_size ( const array < T, N > & )
+inline constexpr size_t indexing_size ( const array < T, N > & )
 
 {
 return N ;
