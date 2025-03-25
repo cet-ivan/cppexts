0f667cecc820c3b150cf69d034bcef7dd45a4a92
cet-ivan <42948108+cet-ivan@users.noreply.github.com>
2025-03-25 03:28:54 +0100

    Improved move assignment implementation of handle

--- source/indir.h
+++ source/indir.h
@@ -1,4 +1,4 @@
-// Copyright Ivan Stanojevic 2023.
+// Copyright Ivan Stanojevic 2025.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // https://www.boost.org/LICENSE_1_0.txt)
@@ -249,8 +249,7 @@ public:
       return * this ; }
 
   handle & operator = ( handle && h ) noexcept
-    { h.swap ( * this ) ;
-      h.clear ( ) ;
+    { handle ( move ( h ) ).swap ( * this ) ;
       return * this ; }
 
   explicit handle ( const T & x ) :
